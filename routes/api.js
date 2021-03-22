cc5f891c8bd2c1c6848b3853025d9c2003d65f13
Eashwar Prabu <eashwarpc@gmail.com>
2021-03-22 12:44:12 +0530

    Made corrections in api

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,32 @@
 const router = require("express").Router();
 
-router.post('/', async(req, res) => {
-    if(req.body.state == 1) {
+router.post("/", async (req, res) => {
+    if (req.body.state == 1) {
         const deviceState = new State({
             id: req.body.id,
             deviceid: req.body.deviceid,
             hubid: req.body.hubid,
             ontime: req.body.time,
             date: req.body.date,
-            day: req,body.day
+            day: req.body.day,
         });
         try {
             const savedState = await deviceState.save();
-            console.log('[On Time Success]');
-            return res.send('Success');
-        } catch(err) {
-            console.log('Error: ' + err);
+            console.log("[On Time Success]");
+            return res.send("Success");
+        } catch (err) {
+            console.log("Error: " + err);
         }
     } else {
         State.updateOne(
             { id: req.body.REQUESTNO },
             { $set: { id: randId, offtime: req.body.TIME } },
-            err => {
-              if (err) return res.status(400).send(err);
-              console.log('[Off Time Success]');
-              return res.send('Success');
+            (err) => {
+                if (err) return res.status(400).send(err);
+                console.log("[Off Time Success]");
+                return res.send("Success");
             }
-          );
+        );
     }
 });
 
